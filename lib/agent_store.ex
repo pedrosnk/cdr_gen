352fac2a1592a1d5adb34692901abbdc50d1e37b
pedrosnk <pedrosnk@gmail.com>
2014-07-27 13:53:53 +0000

    separete some agent functions and remove swp

---
+++ lib/agent_store.ex
@@ -0,0 +1,22 @@
+defmodule AgentStore do
+  @moduledoc """
+  This module will encapsulate mostly common functions
+  used by the cdr fields in order to store, and retrieve for the Agents
+  """
+
+  @doc """
+  returning a list of items to
+  """
+  def list agent_name, generator do
+    case Agent.start_link(fn -> [] end, name: :phone_numbers) do
+      {:ok, _pid} ->
+        phones = generator.()
+        Agent.update :phone_numbers, fn(_) -> phones end
+        phones
+      {:error, _reason_pid} ->
+        Agent.get :phone_numbers,  &(&1)
+    end
+  end
+
+end
+
--- lib/phone_number.ex
+++ lib/phone_number.ex
@@ -2,31 +2,18 @@ defmodule PhoneNumber do
   @moduledoc """
   Module responsile to generate and add phone numbers
   """
-
-  @doc """
-  returning a list of phones that can be used to generate a cdr record
-  """
-  def list_phones do
-    case Agent.start_link(fn -> [] end, name: :phone_numbers) do
-      {:ok, _pid} ->
-        phones = create_phones_list
-        Agent.update :phone_numbers, fn(_) -> phones end
-        phones
-      {:error, _reason_pid} ->
-        Agent.get :phone_numbers,  &(&1)
-    end
-  end
+  import AgentStore
 
   @doc """
   returning a random phone number based on the phone number list that was created
   """
   def get_random do
-    list = list_phones
+    list = list __MODULE__, &generate_list/0
     position = rem round(:random.uniform * 1_000_000 + 1), Enum.count(list)
     Enum.at list, position
   end
 
-  defp create_phones_list do
+  defp generate_list do
     {t1, t2, t3} = :erlang.now
     :random.seed t1, t2, t3
     Enum.map 1..200_000, fn(_) ->
@@ -35,3 +22,4 @@ defmodule PhoneNumber do
   end
 
 end
+
